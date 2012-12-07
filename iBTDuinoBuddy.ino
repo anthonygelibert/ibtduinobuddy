1165618108d4f9017587a0023eee2e8f092e40ad
Anthony Gelibert <anthony.gelibert@me.com>
2012-12-07 06:02:03 +0100

    Update Copyright

--- iBTDuinoBuddy.ino
+++ iBTDuinoBuddy.ino
@@ -1,3 +1,30 @@
+/******************************************************************************
+ * Copyright (c) 2012, Anthony GELIBERT and Jessy GIACOMONI.                  *
+ * All rights reserved.                                                       *
+ *                                                                            *
+ * Redistribution and use in source and binary forms, with or without         *
+ * modification, are permitted provided that the following conditions are met:*
+ *                                                                            *
+ *     * Redistributions of source code must retain the above copyright       *
+ * notice, this list of conditions and the following disclaimer.              *
+ *                                                                            *
+ *     * Redistributions in binary form must reproduce the above copyright    *
+ * notice, this list of conditions and the following disclaimer in the        *
+ * documentation and/or other materials provided with the distribution.       *
+ *                                                                            *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS        *
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT          *
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A    *
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER   *
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,   *
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,        *
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR         *
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY        *
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT               *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE      *
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.       *
+ ******************************************************************************/
+
 #include <Max3421e.h>
 #include <Max3421e_constants.h>
 #include <Usb.h>
@@ -79,7 +106,7 @@ void iBuddyInit(void)
     char buf[0x16] = {0};
 
     /* Copy device 0 endpoint information to device 1. */
-    Usb.setDevTableEntry(IBUDDY_ADDR, Usb.getDevTableEntry(0,0));              
+    Usb.setDevTableEntry(IBUDDY_ADDR, Usb.getDevTableEntry(0,0));
 
     /* Get device description. */
     byte rcode = Usb.getDevDescr(IBUDDY_ADDR, IBUDDY_EP, 0x12 , buf);
@@ -115,9 +142,9 @@ void iBuddyInit(void)
 
 /**
  * Simple function sending to different settings to iBuddy according to <i>i</i> value.
- * 
+ *
  * @param i i==0 => CONFIG 1 // i!=0 => CONFIG 2
- */            
+ */
 void iBuddyTest(int i)
 {
     char header[] = { 0x55, 0x53, 0x42, 0x43, 0x00, 0x40, 0x02, 0xFF };
